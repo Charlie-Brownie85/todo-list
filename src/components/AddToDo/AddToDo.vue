39899da59b0b9f25c70ad4bb5454dc35817ad46c
Charlie-Brownie85 <carlosorbea85@gmail.com>
2024-02-20 10:21:19 +0100

    feat: build AddToDo component and basic logic

--- src/components/AddToDo/AddToDo.vue
+++ src/components/AddToDo/AddToDo.vue
@@ -1,12 +1,21 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 
 import { v4 as uuidv4 } from 'uuid';
+
+import { placeholderExamples } from '@/utils/placeholder-examples';
 
 import { useTodosStore } from '@/stores/todos.ts';
 
 const { addToDo } = useTodosStore();
 const todoText = ref('');
+const placeholderExample = ref(placeholderExamples[Math.floor(Math.random() * placeholderExamples.length)]);
+const placeholder = computed(() => `e.g. ${placeholderExample.value}`);
+
+function clearTodoText() {
+  todoText.value = '';
+  placeholderExample.value = placeholderExamples[Math.floor(Math.random() * placeholderExamples.length)];
+}
 
 function addNewTodo() {
   const newTodo = {
@@ -15,11 +24,37 @@ function addNewTodo() {
     completed: false,
   };
   addToDo(newTodo);
-  todoText.value = '';
+  clearTodoText();
+}
+
+function submitToDo(e: KeyboardEvent) {
+  if (e.key === 'Enter' && todoText.value?.length) {
+    addNewTodo();
+  }
 }
 
 </script>
 <template>
-  <input type="text" v-model="todoText" />
-  <button @click="addNewTodo">Add</button>
+  <input
+    type="text"
+    v-model="todoText"
+    :placeholder="placeholder"
+    @keypress="submitToDo"
+  />
+  <button
+    @click="addNewTodo"
+    :disabled="!todoText.value?.length"
+  >Add</button>
 </template>
+<style scoped lang="postcss">
+input {
+  padding: 0.5rem;
+  margin-right: 0.5rem;
+  border: 1px solid #ccc;
+  border-radius: 0.25rem;
+
+  &::placeholder {
+    @apply text-base-700;
+  }
+}
+</style>
---
+++ src/utils/placeholder-examples.js
@@ -0,0 +1,13 @@
+export const placeholderExamples = [
+  'water the plants',
+  'buy groceries',
+  'call mom',
+  'go for a run',
+  'read a book',
+  'do the laundry',
+  'clean the house',
+  'go to the gym',
+  'cook dinner',
+  'watch a movie',
+  'play the guitar',
+];
