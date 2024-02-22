4c738d2c5e8b54a8cbdffc6012d0d96417b2c61e
Charlie-Brownie85 <carlosorbea85@gmail.com>
2024-02-22 16:52:14 +0100

    feat: add Pinia state persistance

--- src/stores/todos.ts
+++ src/stores/todos.ts
@@ -1,10 +1,14 @@
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
 import { defineStore } from 'pinia';
 
 import type { Todo } from '@/declarations/types';
 
 export const useTodosStore = defineStore('todos', () => {
   const todos = ref<Todo[]>([]);
+
+  if (localStorage.getItem('todos')) {
+    todos.value = JSON.parse(localStorage.getItem('todos') || '[]');
+  }
 
   function addToDo(todo: Todo) {
     todos.value.push(todo);
@@ -29,6 +33,10 @@ export const useTodosStore = defineStore('todos', () => {
       todo.completed = true;
     });
   }
+
+  watch(todos, (newTodos) => {
+    localStorage.setItem('todos', JSON.stringify(newTodos));
+  }, { deep: true });
 
   return {
     todos,
